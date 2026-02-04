d09b0834c765d498a01a9b2d8ace45b130f86334
seenyp <seenyp@gmail.com>
2026-02-04 18:07:04 +0530

    operators

---
+++ Class 6/Arithamitic.js
@@ -0,0 +1,23 @@
+let a=10;
+let b=20.5;
+let c="30";
+let d="rahul";
+let e=true;
+let f=false;
+let g;
+console.log(a+b)
+console.log(a+c)
+console.log(a+d)
+console.log(a+e)
+console.log(a+f)
+console.log(a+g)
+console.log(e+f)
+console.log(a*b)
+console.log(a*c)
+console.log(a*d)
+console.log(a*e)
+console.log(a*f)
+console.log(a*g)
+console.log(e*f)
+
+
---
+++ Class 6/two.js
@@ -0,0 +1,3 @@
+let age=42;
+let esal=42000;
+console.log(esal>=40000 && age<40)
