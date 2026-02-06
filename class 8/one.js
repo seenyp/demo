58da6abada7f9f677d305e05d2b8784282188d07
seenyp <seenyp@gmail.com>
2026-02-06 17:56:44 +0530

    loops

---
+++ class 8/one.js
@@ -0,0 +1,18 @@
+// for(let i=0; i<10;i++){
+// console.log("eggs")
+
+// }
+
+// let i = 0; // Initialize a variable before the loop
+
+// while (i < 5) { // The condition to check before each iteration
+//   console.log(i); // Outputs the current value of i
+//   i++; // Update the variable so the condition eventually becomes false
+// }
+
+// let i = 1;
+
+// do {
+//   console.log(i);
+//   i++; // The counter must be updated inside the loop
+// } while (i <= 7);
